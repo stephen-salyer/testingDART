61643787a1654885a90bf794613e9a376ffe487e
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2020-04-08 16:09:06 -0500

    Routes.js not working but it compiles

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Route, HashRouter, Switch} from 'react-router-dom';
 import Main from './Components/Main';
+import Program from './Components/Program';
 
+/* 
 export default (props) => (
   <HashRouter>
     <Switch>
@@ -10,3 +12,15 @@ export default (props) => (
     </Switch>
   </HashRouter>
 );
+*/
+
+const routes = () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="./Components/program" component={Program} />
+    </Switch>
+  </HashRouter>
+);
+
+export default routes;
