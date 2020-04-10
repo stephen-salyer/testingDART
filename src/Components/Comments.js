32d0b8b9098e8064a001dfb1ecee98611d2ea89a
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2020-04-10 12:50:13 -0500

    Comments Section

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -5,22 +5,11 @@ import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Box from '@material-ui/core/Box';
-
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import SendIcon from '@material-ui/icons/Send';
 import Typography from '@material-ui/core/Typography';
-import Chip from '@material-ui/core/Chip';
-import DoneIcon from '@material-ui/icons/Done';
 import CloseIcon from '@material-ui/icons/Close';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
 import CardHeader from '@material-ui/core/CardHeader';
-import 'date-fns';
+import {TextField, InputAdornment} from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -56,26 +45,6 @@ export default function Comments() {
   const [state, setState] = React.useState({
     right: false,
   });
-  const [age, setAge] = React.useState('');
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-  const handleDelete = () => {
-    console.info('You clicked the delete icon.');
-  };
-
-  const handleClick = () => {
-    console.info('You clicked the Chip.');
-  };
-
-  const [selectedDate, setSelectedDate] = React.useState(
-    new Date('2014-08-18T21:11:54')
-  );
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -101,7 +70,7 @@ export default function Comments() {
         alignItems="center"
         justifyContent="space-between"
       >
-        <CardHeader title="Filter Programs"></CardHeader>
+        <CardHeader title="Comments"></CardHeader>
         <Box p={1}>
           <IconButton onClick={toggleDrawer(anchor, false)} color="inherit">
             <CloseIcon />
@@ -109,190 +78,33 @@ export default function Comments() {
         </Box>
       </Box>
       <Box pb={2} pl={2} pr={2} pt={0}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Search Members
-          </InputLabel>
-
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Bob Bobbert</MenuItem>
-            <MenuItem value={20}>Rob Robbert</MenuItem>
-            <MenuItem value={30}>Tob Tobbert</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <Typography variant="subtitle1">Years</Typography>
-        <Chip
-          onClick={handleClick}
-          className={classes.chipPadding}
-          label="2022"
-        />
-        <Chip
-          onClick={handleClick}
-          className={classes.chipPadding}
-          label="2021"
-        />
-        <Chip
-          className={classes.chipPadding}
-          label="2020"
-          onClick={handleClick}
-          onDelete={handleDelete}
-          deleteIcon={<DoneIcon />}
-        />
-        <Chip
-          onClick={handleClick}
-          className={classes.chipPadding}
-          label="2019"
-        />
-        <Chip
-          onClick={handleClick}
-          className={classes.chipPadding}
-          label="2018"
-        />
-        <Chip
-          onClick={handleClick}
-          className={classes.chipPadding}
-          label="2017"
+        <TextField
+          className={(classes.margin, classes.formControl)}
+          id="input-with-icon-textfield"
+          label="Comment"
+          variant="outlined"
+          multiline
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SendIcon />
+              </InputAdornment>
+            ),
+          }}
         />
       </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">Region</InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem>
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>North America</MenuItem>
-          </Select>
-        </FormControl>
+      <Box pl={2} pr={2} display="flex" justifyContent="space-between">
+        <Typography variant="subtitle2">Evan Kommenter</Typography>
+        <Typography variant="caption">01/01/20 @1:11PM</Typography>
       </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Country
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>US</MenuItem>
-            <MenuItem value={20}>Canada</MenuItem>
-          </Select>
-        </FormControl>
+      <Box mt={-0.5} pb={1} pl={2} pr={2}>
+        <Typography variant="caption">Tower ABC-123</Typography>
       </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Business Category
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Channel</MenuItem>
-            <MenuItem value={20}>Climate</MenuItem>
-            <MenuItem value={30}>Crop Protection</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Product
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>All</MenuItem>
-            <MenuItem value={20}>Acceleration</MenuItem>
-            <MenuItem value={30}>Acetanilide</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">Brand</InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Search Members"
-            value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>All</MenuItem>
-            <MenuItem value={20}>Absolute</MenuItem>
-            <MenuItem value={30}>Absolute Maxx</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box pb={2} pl={2} pr={2}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            disableToolbar
-            fullWidth={true}
-            variant="inline"
-            format="MM/dd/yyyy"
-            margin="normal"
-            id="date-picker-inline"
-            label="Communication Date"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-        </MuiPickersUtilsProvider>
-      </Box>
-      <Box p={2} display="flex" justifyContent="flex-end">
-        <Box pr={1}>
-          <Button>clear</Button>
-        </Box>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={toggleDrawer(anchor, false)}
-          onKeyDown={toggleDrawer(anchor, false)}
-        >
-          Apply Filters
-        </Button>
+      <Box mt={0} pb={2} pl={2} pr={2}>
+        <Typography variant="caption" lineHeight="8px">
+          This is a comment that is not real. It takes up space. Its a happy
+          little comment.
+        </Typography>
       </Box>
     </div>
   );
@@ -301,7 +113,6 @@ export default function Comments() {
     <Box
       display="flex"
       alignItems="center"
-      onClick={toggleDrawer('right', true)}
       className={classes.CommentsButtonMargin}
     >
       <Button
