d014cf06a9d8558242ee5df1901e59b4a9f86391
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2020-04-20 14:21:14 -0500

    Members List

--- src/Components/MemberList.js
+++ src/Components/MemberList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {
   withStyles,
   makeStyles,
@@ -23,9 +23,22 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Toolbar,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
 } from '@material-ui/core';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import PublishIcon from '@material-ui/icons/Publish';
+import RemoveIcon from '@material-ui/icons/RemoveCircle';
 import DeleteScopeModal from './DeleteScopeModal';
 import SearchBar from './SearchBar';
 
@@ -65,16 +78,29 @@ const useStyles = makeStyles((theme) => ({
   relatedContentPadding: {
     padding: '0 0 0 8px',
   },
-  content: {
-    '&$expanded': {
-      margin: '12px 0',
-    },
-  },
   formControl: {
     margin: 0,
     fullWidth: true,
     display: 'flex',
     wrap: 'nowrap',
+  },
+  toolBarEnd: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  listItemCorrection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  listItemTertiary: {
+    marginTop: '0',
+    fontWeight: '400',
+    color: 'rgba(0, 0, 0, 0.54)',
+    fontSize: '0.875rem',
+  },
+  listItemTextNoMargin: {
+    marginBottom: '0',
   },
 }));
 
@@ -128,38 +154,46 @@ const Members = [
   {title: 'Kinsley Christian'},
 ];
 
+const Department = [
+  {title: 'Audit'},
+  {title: 'Business'},
+  {title: 'Credit'},
+  {title: 'Finance'},
+  {title: 'Global Accounting'},
+  {title: 'IT'},
+  {title: 'Legal'},
+  {title: 'Licensee'},
+  {title: 'Marketing'},
+  {title: 'MF Analytics'},
+  {title: 'Operations'},
+  {title: 'Product Manager'},
+  {title: 'Sales'},
+];
+
 const scopeData = [
   {
     id: 1,
-    region: 'North America',
-    country: 'United States of America',
-    businessCategory: 'Crop Protection',
-    product: 'Glyphosate',
-    brand: 'All',
+    region: 'Europe / Middle East / Africa',
+    country: 'All Countries',
+    businessCategory: 'All Business Categories',
+    product: 'All Products',
+    brand: 'All Brands',
   },
   {
     id: 2,
-    region: 'North America',
-    country: 'United States of America',
-    businessCategory: 'Crop Protection',
-    product: 'Glyphosate',
-    brand: 'Roundup',
+    region: 'Europe / Middle East / Africa',
+    country: 'Austria',
+    businessCategory: 'All Business Categories',
+    product: 'All Products',
+    brand: 'All Brands',
   },
   {
     id: 3,
-    region: 'North America',
-    country: 'United States of America',
-    businessCategory: 'Crop Protection',
-    product: 'Glyphosate',
-    brand: 'Degree Xtra',
-  },
-  {
-    id: 4,
-    region: 'North America',
-    country: 'United States of America',
-    businessCategory: 'Crop Protection',
-    product: 'Dicamba',
-    brand: 'Roundup Xtra',
+    region: 'Europe / Middle East / Africa',
+    country: 'Belarus',
+    businessCategory: 'All Business Categories',
+    product: 'All Products',
+    brand: 'All Brands',
   },
 ];
 
@@ -176,7 +210,8 @@ const inputs = [
     label: 'Region',
     options: [
       'Asia / Pacific',
-      'Europe / Middle East',
+      'Austria',
+      'Europe / Middle East / Africa',
       'North America',
       'South America',
     ],
@@ -185,19 +220,26 @@ const inputs = [
   {
     key: 'country',
     label: 'Country',
-    options: ['Canada', 'Brazil', 'United States of America'],
+    options: [
+      'All Countries',
+      'Austria',
+      'Belarus',
+      'Canada',
+      'Brazil',
+      'United States of America',
+    ],
     size: 6,
   },
   {
     key: 'businessCategory',
     label: 'Business Category',
-    options: ['All', 'Crop Protection', 'Pasture'],
+    options: ['All Business Categories', 'Crop Protection', 'Pasture'],
     size: 4,
   },
   {
     key: 'product',
     label: 'Product',
-    options: ['All', 'Dicamba', 'Glyphosate', 'Other'],
+    options: ['All Products', 'Dicamba', 'Glyphosate', 'Other'],
     size: 4,
   },
   {
@@ -223,34 +265,36 @@ const MemberList = () => {
     <ThemeProvider theme={theme}>
       <Container maxWidth="lg" className={classes.containerPadding}>
         <Grid container spacing={3} direction="row">
-          <Grid item={true} sm={12} md={6}>
-            <Typography variant="h3" color="primary">
+          <Toolbar
+            disableGutters={true}
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="h3" color="primary" edge="start">
               Member List
             </Typography>
-          </Grid>
-          <Grid item={true} sm={12} md={2}>
-            <Box display="flex" justifyContent="flex-end" flexWrap="wrap">
-              <Autocomplete
-                id="combo-box-demo"
-                options={Members}
-                getOptionLabel={(option) => option.title}
-                style={{width: '100%'}}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Search Members"
-                    variant="outlined"
-                  />
-                )}
-              />
-            </Box>
-          </Grid>
-          <Grid item={true} sm={12} md={4}>
-            <Box display="flex">
+            <Box display="flex" justifyContent="flex-end">
+              <Box width="300px">
+                <Autocomplete
+                  options={Members}
+                  getOptionLabel={(option) => option.title}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Search Members"
+                      variant="outlined"
+                    />
+                  )}
+                />
+              </Box>
               <Button
                 color="primary"
                 className={classes.button}
                 endIcon={<GetAppIcon />}
+                edge="end"
               >
                 Export Scope
               </Button>
@@ -258,55 +302,166 @@ const MemberList = () => {
                 color="primary"
                 className={classes.button}
                 endIcon={<GetAppIcon />}
+                edge="end"
               >
                 Export To Excel
               </Button>
             </Box>
-          </Grid>
-          <Grid container spacing={3} style={{margin: '24px 0 0 0'}}>
-            <Grid item={true} sm={12} md={12}>
-              <Box display="flex" alignItems="baseline">
-                <Typography variant="h5">Raul Abrego</Typography>
-                <Typography
-                  variant="subtitle1"
-                  className={classes.relatedContentPadding}
-                >
-                  (RRABRE)
-                </Typography>
-              </Box>
-              <Grid item={true} sm={12} md={12}>
-                C&C Specialist
-              </Grid>
-              <Grid item={true} sm={12} md={12}>
-                Security Groups: D-A-R-T Edit, Dart Edit NA
-              </Grid>
+          </Toolbar>
+        </Grid>
+        <Grid container style={{margin: '24px 0 0 0'}}>
+          <Grid item={true} xs={12} md={12}>
+            <Box display="flex" alignItems="baseline">
+              <Typography variant="h5">Raul Abrego</Typography>
+              <Typography
+                variant="subtitle1"
+                className={classes.relatedContentPadding}
+              >
+                (RRABRE)
+              </Typography>
+            </Box>
+            <Grid item={true} xs={12} md={12}>
+              C&C Specialist
+            </Grid>
+            <Grid item={true} xs={12} md={12}>
+              Security Groups: D-A-R-T Edit, Dart Edit NA
             </Grid>
           </Grid>
-          <Grid container spacing={3} style={{margin: '24px 0 0 0'}}>
-            <Grid item={true} sm={12} md={4}>
+        </Grid>
+        <Grid container spacing={3} style={{marginTop: '24px'}}>
+          <Grid container item xs={12} md={4}>
+            <Grid item={true} xs={12} md={12}>
               <Card>
                 <CardHeader
                   titleTypographyProps={{variant: 'h6'}}
                   title="User Details"
                 ></CardHeader>
                 <Divider />
-                <Box p={2}>
+                <Box pt={2} pr={2} pl={2}>
                   <TextField
-                    id="outlined-basic"
                     label="DOA"
                     defaultValue="222,222,255"
                     variant="outlined"
                     fullWidth={true}
                   />
                 </Box>
+                <Box pt={2} pr={2} pl={2}>
+                  <TextField
+                    label="DOA Length in Years"
+                    defaultValue="1"
+                    variant="outlined"
+                    fullWidth={true}
+                  />
+                </Box>
+                <Box pt={2} pr={2} pl={2}>
+                  <FormControl
+                    component="fieldset"
+                    className={classes.formControlRadio}
+                  >
+                    <FormLabel component="legend">
+                      Program Classification Access
+                    </FormLabel>
+                    <RadioGroup aria-label="quiz" name="quiz">
+                      <FormControlLabel
+                        value="best"
+                        control={<Radio />}
+                        label="All Agreements"
+                      />
+                      <FormControlLabel
+                        value="worst"
+                        control={<Radio />}
+                        label="Non-Strategic Agreement"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Box>
+                <Box p={2}>
+                  <Autocomplete
+                    freeSolo
+                    options={Department}
+                    getOptionLabel={(option) => option.title}
+                    style={{width: '100%'}}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Department"
+                        variant="outlined"
+                      />
+                    )}
+                  />
+                </Box>
               </Card>
             </Grid>
-            <Grid item={true} sm={12} md={8}>
+            <Grid item={true} xs={12} md={12} style={{paddingTop: '24px'}}>
               <Card>
-                <Box display="flex" justifyContent="space-between">
-                  <CardHeader title="Programs"></CardHeader>
-                  <SearchBar />
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  pr={1}
+                >
+                  <CardHeader
+                    titleTypographyProps={{variant: 'h6'}}
+                    title="Attachments"
+                  ></CardHeader>
+                  <IconButton color="primary" style={{height: '48px'}}>
+                    <PublishIcon />
+                  </IconButton>
                 </Box>
+                <Divider />
+                <Box>
+                  <List disablePadding={true}>
+                    {[1, 2].map((n) => (
+                      <Fragment key={n}>
+                        <ListItem className={classes.listItemCorrection}>
+                          <ListItemText
+                            className={classes.listItemTextNoMargin}
+                            primary={
+                              <Typography variant="overline">
+                                Bill Attacher • 01/01/22
+                              </Typography>
+                            }
+                            secondary={
+                              <Typography variant="body1">
+                                This-File-Name-Could-Be-Very-Long-Possibly-Even-60-Characters.doc
+                              </Typography>
+                            }
+                          />
+                          <ListItemText
+                            classes={{primary: classes.listItemTertiary}}
+                            primary={
+                              <Typography variant="caption">
+                                I uploaded this file so that it can be seen by
+                                others
+                              </Typography>
+                            }
+                          />
+                          <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="morevert">
+                              <RemoveIcon />
+                            </IconButton>
+                          </ListItemSecondaryAction>
+                        </ListItem>
+                        <Divider />
+                      </Fragment>
+                    ))}
+                  </List>
+                </Box>
+              </Card>
+            </Grid>
+          </Grid>
+          <Grid container item={true} xs={12} md={8}>
+            <Grid item={true} md={12}>
+              <Card style={{height: '767px'}}>
+                <Toolbar className={classes.toolBarEnd} disableGutters={true}>
+                  <CardHeader
+                    titleTypographyProps={{variant: 'h6'}}
+                    title="Scope Builder"
+                  ></CardHeader>
+                  <div className={classes.toolBarEnd}>
+                    <SearchBar />
+                  </div>
+                </Toolbar>
                 <Divider />
                 {scopes.map((scope) => (
                   <ExpansionPanel
@@ -323,7 +478,7 @@ const MemberList = () => {
                       <div className={classes.root}>
                         <Grid container spacing={3}>
                           {inputs.map(({label, options, size, key}, i) => (
-                            <Grid key={i} item sm={12} md={size}>
+                            <Grid key={i} item xs={12} md={size}>
                               <FormControl
                                 variant="outlined"
                                 className={classes.formControl}
@@ -353,6 +508,11 @@ const MemberList = () => {
                     <Divider />
                   </ExpansionPanel>
                 ))}
+                <Box pt={1} pb={1} display="flex" justifyContent="center">
+                  <Button color="primary" endIcon={<AddIcon />}>
+                    Add New scope
+                  </Button>
+                </Box>
               </Card>
             </Grid>
           </Grid>
