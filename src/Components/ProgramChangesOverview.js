8cb67331a61bd9cdb2a8ceab26c697d27223d41d
epyiv <stephen.salyer.ext@bayer.com>
2020-06-29 17:02:20 -0500

    Diff updates

--- src/Components/ProgramChangesOverview.js
+++ src/Components/ProgramChangesOverview.js
@@ -30,6 +30,91 @@ const useStyles = makeStyles({
   },
 });
 
+const overviewChanges = [
+  {
+    version: 'Version',
+    items: [
+      {
+        primary: 'Program Name',
+        secondaryTakeDiff: (
+          <>
+            <Typography>
+              <span
+                style={{
+                  backgroundColor: red[100],
+                  textDecoration: 'line-through',
+                }}
+              >
+                An Extremely
+              </span>{' '}
+              Long Program Name{' '}
+              <span
+                style={{
+                  backgroundColor: red[100],
+                  textDecoration: 'line-through',
+                }}
+              >
+                123123123123123
+              </span>
+            </Typography>
+          </>
+        ),
+        whoChanged: 'Bob Boberson - YYYY/MM/DD',
+      },
+      {
+        primary: 'Program ID',
+        secondaryTakeDiff: (
+          <>
+            <Typography>Program ID</Typography>
+          </>
+        ),
+        whoChanged: 'Bob Boberson - YYYY/MM/DD',
+      },
+    ],
+  },
+  {
+    version: 'Version',
+    items: [
+      {
+        primary: 'Program Name',
+        secondaryTakeDiff: (
+          <>
+            <Typography>
+              <span
+                style={{
+                  backgroundColor: red[100],
+                  textDecoration: 'line-through',
+                }}
+              >
+                An Extremely
+              </span>{' '}
+              Long Program Name{' '}
+              <span
+                style={{
+                  backgroundColor: red[100],
+                  textDecoration: 'line-through',
+                }}
+              >
+                123123123123123
+              </span>
+            </Typography>
+          </>
+        ),
+        whoChanged: 'Bob Boberson - YYYY/MM/DD',
+      },
+      {
+        primary: 'Program ID',
+        secondaryTakeDiff: (
+          <>
+            <Typography>Program ID</Typography>
+          </>
+        ),
+        whoChanged: 'Bob Boberson - YYYY/MM/DD',
+      },
+    ],
+  },
+];
+
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const menuProps = {
@@ -51,6 +136,7 @@ export default function ProgramChangesOverview() {
   return (
     <div className={classes.root}>
       <Grid container>
+        {/*
         <Grid item xs={6}>
           <Box className={classes.flexPositioning}>
             <FormControl
@@ -204,6 +290,54 @@ export default function ProgramChangesOverview() {
             </Grid>
           </Grid>
         </Box>
+        this is where the change happens */}
+        {overviewChanges.map(({version, items}) => (
+          <>
+            <Grid key={version} item xs={6}>
+              <Box className={classes.flexPositioning}>
+                <FormControl
+                  variant="outlined"
+                  className={classes.formControl}
+                  margin="dense"
+                  label={version}
+                >
+                  <InputLabel htmlFor="outlined-age-native-simple">
+                    {version}
+                  </InputLabel>
+                  <Select
+                    style={{height: 37}}
+                    MenuProps={menuProps}
+                    defaultValue={20}
+                    id="demo-simple-select-outlined"
+                    label="Version"
+                  >
+                    <MenuItem value={10}>1.1</MenuItem>
+                    <MenuItem value={20}>1.2</MenuItem>
+                    <MenuItem value={30}>1.3</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              {items.map(({primary, secondaryTakeDiff, whoChanged}, i) => (
+                <>
+                  <ListItemText
+                    key={i}
+                    primary={primary}
+                    secondary={secondaryTakeDiff}
+                  />
+                  <Divider />
+                  <FormHelperText>{whoChanged}</FormHelperText>
+                  <Box pl={3} pr={3}>
+                    <Divider
+                      style={{height: '100%', width: 1}}
+                      orientation="vertical"
+                      flexItem
+                    />
+                  </Box>
+                </>
+              ))}
+            </Grid>
+          </>
+        ))}
       </Grid>
     </div>
   );
